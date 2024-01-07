47e4bda38e14e1347e73981fae98b176a5c5f56b
Belati <belatijagadbintangsyuhada@gmail.com>
2024-01-07 17:02:02 +0700

    Cleaning about page

---
+++ src/app/about/constants.ts
@@ -0,0 +1,30 @@
+export const education = [
+    { content: 'Undergraduate Computer Science Student', institution: 'University of Indonesia', period: 'Aug 2022 - Now', link: '' },
+]
+
+export const experience = [
+    { content: 'VPIC of Automation Engineer', institution: 'COMPFEST 15', period: 'Dec 2022 - Nov 2023', link: '' },
+    { content: 'Website Developer', institution: 'Educare', period: 'Nov 2022 - Mar 2023', link: '' },
+]
+
+export const honors = [
+    { content: 'Dataslayer 1.0 Finalist', institution: 'BEM ITT Purwokerto', period: 'Jan 2024', link: '' },
+    { content: 'Airnology 2.0: Innovation Quest 1st Place', institution: 'BEM FTMM Unair', period: 'Oct 2023', link: 'https://www.instagram.com/airnology2.0/' },
+    { content: 'Airnology 2.0: Objective Quest 3rd Place', institution: 'BEM FTMM Unair', period: 'Oct 2023', link: 'https://www.instagram.com/airnology2.0/' },
+    { content: 'Satria Data 2023: Big Data Challenge Semifinalist', institution: 'Puspresnas Kemendikbud', period: 'July 2023', link: 'https://satriadata.kemdikbud.go.id/' },
+    { content: 'JOINTS: Data Competition Finalist', institution: 'OmahTI UGM', period: 'May 2023', link: 'https://www.instagram.com/jointsugm/' },
+    { content: 'Arkavidia 7.0: Datavidia 2nd Place ', institution: 'HMIF ITB', period: 'Feb 2023', link: 'https://www.instagram.com/arkavidia/' },
+    { content: 'Gemastik XV: Data Mining Finalist', institution: 'Puspresnas Kemendikbud', period: 'Aug 2022', link: 'https://gemastik.kemdikbud.go.id/' },
+    { content: 'Satria Data 2022: Big Data Challenge Semifinalist', institution: 'Puspresnas Kemendikbud', period: 'July 2022', link: 'https://satriadata.kemdikbud.go.id/' },
+]
+
+export const certifications = [
+    { content: 'IELTS Band 8.0', institution: 'IDP Indonesia', period: 'Oct 2022', link: '' },
+]
+
+export const sections = [
+    { name: 'Education', section: education },
+    { name: 'Experience', section: experience },
+    { name: 'Certifications', section: certifications },
+    { name: 'Honors', section: honors },
+]
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import { sections } from './constants'
 
 interface Content {
     content: string,
@@ -7,42 +9,38 @@ interface Content {
     link?: string,
 }
 
-const education = [
-    { content: 'Undergraduate Computer Science Student', institution: 'University of Indonesia', period: 'Aug 2022 - Now', link: '' },
-]
-
-const experience = [
-    { content: 'VPIC of Automation Engineer', institution: 'COMPFEST 15', period: 'Dec 2022 - Nov 2023', link: '' },
-    { content: 'Website Developer', institution: 'Educare', period: 'Nov 2022 - Mar 2023', link: '' },
-]
-
-const honors = [
-    { content: 'Dataslayer 1.0 Finalist', institution: 'BEM ITT Purwokerto', period: 'Jan 2024', link: '' },
-    { content: 'Airnology 2.0: Innovation Quest 1st Place', institution: 'BEM FTMM Unair', period: 'Oct 2023', link: 'https://www.instagram.com/airnology2.0/' },
-    { content: 'Airnology2.0: Objective Quest 3rd Place', institution: 'BEM FTMM Unair', period: 'Oct 2023', link: 'https://www.instagram.com/airnology2.0/' },
-    { content: 'Satria Data 2023: Big Data Challenge Semifinalist', institution: 'Puspresnas Kemendikbud', period: 'July 2023', link: 'https://satriadata.kemdikbud.go.id/' },
-    { content: 'JOINTS: Data Competition Finalist', institution: 'OmahTI UGM', period: 'May 2023', link: 'https://www.instagram.com/jointsugm/' },
-    { content: 'Arkavidia 7.0: Datavidia 2nd Place ', institution: 'HMIF ITB', period: 'Feb 2023', link: 'https://www.instagram.com/arkavidia/' },
-    { content: 'Gemastik XV: Data Mining Finalist', institution: 'Puspresnas Kemendikbud', period: 'Aug 2022', link: 'https://gemastik.kemdikbud.go.id/' },
-    { content: 'Satria Data 2022: Big Data Challenge Semifinalist', institution: 'Puspresnas Kemendikbud', period: 'July 2022', link: 'https://satriadata.kemdikbud.go.id/' },
-]
-
-const certifications = [
-    { content: 'IELTS Band 8.0', institution: 'IDP Indonesia', period: 'Oct 2022', link: '' },
-]
-
-const sections = [
-    { name: 'Education', section: education },
-    { name: 'Experience', section: experience },
-    { name: 'Certifications', section: certifications },
-    { name: 'Honors', section: honors },
-]
-
 export default function About() {
     return(
         <main className="container mx-auto">
-            <div className="min-h-screen">
-                Ini tentang diriku
+            <div className="min-h-screen grid grid-cols-12 gap-8 items-center">
+                <div className="col-span-3 col-start-3">
+                    <Image 
+                        src="/placeholder_profile.jpg" 
+                        alt="My portrait"
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        style={{ width: '100%', height: 'auto' }} 
+                    />
+                </div>
+                <div className="col-span-6 flex flex-col gap-8 text-lg">
+                    <p>
+                        My name is Belati Jagad Bintang Syuhada, usually called by my nickname Abel. 
+                        I&apos;m an undergraduate student pursuing computer science degree in University of Indonesia.
+                        Currently in my fourth semester, I&apos;m diligently strengthening my foundation of computer science
+                        in order to learn more about machine learning and artificial intelligence.
+                    </p>
+                    <p>
+                        I dream of becoming a research scientist in the field of machine learning and artificial intelligence
+                        in hope of making new breakthroughs that will impact the field in a positive way.
+                        To achieve that dream, I participated in many competitions in order to deepen my insight about the true
+                        meaning of machine learning.
+                    </p>
+                    <p>
+                        My interest beside studying machine learning is reading japanese/chinese/korean novels and comics, or well
+                        known as manga, manhua, and manhwas.
+                    </p>
+                </div>
             </div>
             {sections.map((section) => (
                 <Section key={section.name} title={section.name} contents={section.section} />
